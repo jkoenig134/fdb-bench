37fdb30ebedd11917a408f480e2c3818a6e496c4
Julian König <julian.koenig@js-soft.com>
2025-01-23 19:55:42 +0100

    feat: update benchs for ferretdb 2

--- index.ts
+++ index.ts
@@ -7,15 +7,10 @@ import { fill } from "./fill.js"
 const dbName = "acc1"
 const collectionName = "c"
 
-const fast = new MongoDbConnection("mongodb://localhost:27017")
-await fast.connect()
-const fastDb = await fast.getDatabase(dbName)
-const fastCollection = await fastDb.getCollection(collectionName)
-
-const slow = new MongoDbConnection("mongodb://localhost:27018")
-await slow.connect()
-const slowDb = await slow.getDatabase(dbName)
-const slowCollection = await slowDb.getCollection(collectionName)
+const ferret = new MongoDbConnection("mongodb://localhost:27017")
+await ferret.connect()
+const ferretDb = await ferret.getDatabase(dbName)
+const ferretCollection = await ferretDb.getCollection(collectionName)
 
 const mongodb = new MongoDbConnection("mongodb://root:example@localhost:27019")
 await mongodb.connect()
@@ -28,32 +23,26 @@ const numberOfDocsPerIteration = 1000
 const timings: Record<number, Timings> = {}
 
 for (let i = 0; i < iterations; i++) {
-  await fill(numberOfDocsPerIteration, fastCollection)
+  await fill(numberOfDocsPerIteration, ferretCollection)
   await fill(numberOfDocsPerIteration, mongodbCollection)
 
-  const randomFerretPeer = (await fastCollection.findOne()).peer.address
-  const count = await fastCollection.count()
+  const randomFerretPeer = (await ferretCollection.findOne()).peer.address
+  const count = await ferretCollection.count()
 
-  const fast = await bench(
-    `fast with ${count} docs`,
-    async () => await fastCollection.find({ "peer.address": randomFerretPeer }),
+  const ferretResults = await bench(
+    `ferret with ${count} docs`,
+    async () => await ferretCollection.find({ "peer.address": randomFerretPeer }),
     10
   )
 
-  const slow = await bench(
-    `slow with ${count} docs`,
-    async () => await slowCollection.find({ "peer.address": randomFerretPeer }),
-    3
-  )
-
-  const randomNativePeer = (await fastCollection.findOne()).peer.address
-  const native = await bench(
+  const randomNativePeer = (await ferretCollection.findOne()).peer.address
+  const nativeResults = await bench(
     `native with ${count} docs`,
     async () => await mongodbCollection.find({ "peer.address": randomNativePeer }),
     10
   )
 
-  timings[count] = { fast, slow, native }
+  timings[count] = { ferret: ferretResults, native: nativeResults }
 
   console.log("------------------------------------------------------------")
 }
@@ -61,10 +50,9 @@ for (let i = 0; i < iterations; i++) {
 console.log(
   table([
     ["Pushdown /\nCount", "Enabled", "Disabled", "Native"],
-    ...Object.entries(timings).map(([count, { fast, slow, native }]) => [
+    ...Object.entries(timings).map(([count, { ferret, native }]) => [
       count,
-      `min: ${fast.min.toFixed(2)}ms\nmax: ${fast.max.toFixed(2)}ms\navg: ${fast.avg.toFixed(2)}ms`,
-      `min: ${slow.min.toFixed(2)}ms\nmax: ${slow.max.toFixed(2)}ms\navg: ${slow.avg.toFixed(2)}ms`,
+      `min: ${ferret.min.toFixed(2)}ms\nmax: ${ferret.max.toFixed(2)}ms\navg: ${ferret.avg.toFixed(2)}ms`,
       `min: ${native.min.toFixed(2)}ms\nmax: ${native.max.toFixed(2)}ms\navg: ${native.avg.toFixed(2)}ms`,
     ]),
   ])
@@ -74,10 +62,7 @@ const nf = new Intl.NumberFormat()
 fs.writeFileSync(
   "timings.csv",
   Object.entries(timings)
-    .map(
-      ([num, timing]) =>
-        `${num};${nf.format(timing.fast.avg)};${nf.format(timing.slow.avg)};${nf.format(timing.native.avg)}`
-    )
+    .map(([num, timing]) => `${num};${nf.format(timing.ferret.avg)};${nf.format(timing.native.avg)}`)
     .join("\n")
 )
 fs.writeFileSync("timings.json", JSON.stringify(timings, null, 2))
